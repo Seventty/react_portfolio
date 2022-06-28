8bd250a50c8b11d68bf7affd5af1bf41cc6f0157
seventty <elmastigeron2015@gmail.com>
2022-06-28 01:44:27 -0400

    About me and experiences components added

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React from "react";
 import "./about.css";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+const mainAboutImage = require("../../assets/me-about.jpg");
 
 const About = () => {
   return (
-    <section id="about">About</section>
-  )
-}
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About me</h2>
 
-export default About
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={mainAboutImage} alt="AboutImage" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3+ years developing</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>4 Clients</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>Look at my GitHub</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+            deleniti quisquam, laboriosam nemo quibusdam explicabo odio minima
+            impedit veniam, quia ipsam eos beatae unde molestiae hic
+            consequuntur, nesciunt quo mollitia!
+          </p>
+          <a href="#contact" className="btn btn-primary">Lets Talk</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,8 +1,45 @@
 import React from "react";
 import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
+import { GiHexagonalNut } from "react-icons/gi";
+import { FaReact } from "react-icons/fa";
 
 const Experience = () => {
-  return <section id="experience">Experience</section>;
+  return (
+    <section id="experience">
+      <h5>Skills That I Have</h5>
+      <h2>My Knifes</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <FaReact className="header__icon"/>
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__detai">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+          <GiHexagonalNut className="header__icon"/>
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__detai">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
