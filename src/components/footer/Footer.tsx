e67053849be3f96f110593c1f3636ad0d6923e92
seventty <elmastigeron2015@gmail.com>
2022-06-29 21:11:46 -0400

    footer component added

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import "./footer.css"
+import React from "react";
+import "./footer.css";
+import { BsTelegram, BsLinkedin, BsSpotify } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <a href="#/" className="footer__logo">Rainiery Valerio</a>
 
-export default Footer
+      <ul className="permalinks">
+        <li><a href="#/">Home</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://google.com"><BsTelegram/></a>
+        <a href="https://google.com"><BsLinkedin/></a>
+        <a href="https://google.com"><BsSpotify/></a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy; Rainiery Valerio Gonzalez, All rights reserved.</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
