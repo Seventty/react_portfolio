89994435e7486e8179a9203dafb30889f6beec8b
seventty <elmastigeron2015@gmail.com>
2022-07-04 17:34:32 -0400

    portfolio profile added

--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -11,10 +11,10 @@ import "swiper/css/navigation";
 const data = [
   {
     id: 1,
-    avatar: "https://i.imgur.com/GzZAVFv.png",
+    avatar: "https://i.imgur.com/KOW9Xe2.png",
     name: "Honil Cristina Barinas Nuñez",
     review:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem perspiciatis at voluptates quod, facere odio! Veritatis distinctio ullam commodi quisquam nam unde natus, harum qui fugit fuga minima sit alias!",
+      "Multimedia Freelancer, IlustrandoAndo CEO\nTrabajar con Rainiery Valerio y llevar a cabo la gestión de un proyecto, ha sido una de las cosas más gratificantes de vivir en el mundo del software, por el sólo hecho de que busca las mejores soluciones y valora el término 'Calidad' , lo cual como todo sabemos hace que aún un simple trabajo, se convierta en una experiencia de creación.",
   },
 ];
 
